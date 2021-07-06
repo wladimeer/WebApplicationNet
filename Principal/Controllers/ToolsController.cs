1e5ce8a0cca550d8b4ad9fb3f7dac3a9e9ba5f52
Sebastián <wladimeerbc@gmail.com>
2021-07-06 17:24:25 -0400

    Add project files.

---
+++ Principal/Controllers/ToolsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Principal.Controllers
+{
+    public class ToolsController : Controller
+    {
+        // GET: Tools
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
